c23e2be8ad31b530a15e810d25a322deae6e82a3
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-03-01 20:03:09 -0500

    Add project files.

---
+++ CSV2QR/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CSV2QR
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFileBtn = new System.Windows.Forms.Button();
+            this.FileNameLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SelectFileBtn
+            // 
+            this.SelectFileBtn.Location = new System.Drawing.Point(281, 82);
+            this.SelectFileBtn.Name = "SelectFileBtn";
+            this.SelectFileBtn.Size = new System.Drawing.Size(117, 23);
+            this.SelectFileBtn.TabIndex = 0;
+            this.SelectFileBtn.Text = "Select File";
+            this.SelectFileBtn.UseVisualStyleBackColor = true;
+            this.SelectFileBtn.Click += new System.EventHandler(this.SelectFileBtn_Click);
+            // 
+            // FileNameLbl
+            // 
+            this.FileNameLbl.AutoSize = true;
+            this.FileNameLbl.Location = new System.Drawing.Point(448, 82);
+            this.FileNameLbl.Name = "FileNameLbl";
+            this.FileNameLbl.Size = new System.Drawing.Size(85, 13);
+            this.FileNameLbl.TabIndex = 1;
+            this.FileNameLbl.Text = "No File Selected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FileNameLbl);
+            this.Controls.Add(this.SelectFileBtn);
+            this.Name = "Form1";
+            this.Text = "Excel2QR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFileBtn;
+        private System.Windows.Forms.Label FileNameLbl;
+    }
+}
+
---
+++ CSV2QR/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV2QR
+{
+    public partial class Form1 : Form
+    {
+        private const string _noFileSelectedMsg = "No File Selected";
+        public Form1()
+        {
+            InitializeComponent();
+            FileNameLbl.Text = _noFileSelectedMsg;
+        }
+
+        private void SelectFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                Multiselect = false,
+                Filter = "Excel files |*.xlsx;*.xls"
+            };
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    FileNameLbl.Text = openFileDialog1.SafeFileName;
+                    var fileStream = openFileDialog1.OpenFile();
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+            if(result == DialogResult.Cancel)
+            {
+                FileNameLbl.Text = _noFileSelectedMsg;
+            }
+        }
+    }
+}
