2112a9136d99743fb53ad13de7b7967f54c528f3
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-04-04 12:38:37 -0400

    add test file, clean up code

--- CSV2QR/Form1.cs
+++ CSV2QR/Form1.cs
@@ -19,35 +19,6 @@ namespace CSV2QR
         private const string _noFileSelectedMsg = "No File Selected";
         private OpenFileDialog _openFileDialog;
         private readonly bool _isDotNetCoreApp;
-        private readonly Dictionary<int, string> _excelCols = new Dictionary<int, string>
-        {
-            {0, "A" },
-            {1, "B" },
-            {2, "C" },
-            {3, "D" },
-            {4, "E" },
-            {5, "F" },
-            {6, "G" },
-            {7, "H" },
-            {8, "I" },
-            {9, "J" },
-            {10, "K" },
-            {11, "L" },
-            {12, "M" },
-            {13, "N" },
-            {14, "O" },
-            {15, "P" },
-            {16, "Q" },
-            {17, "R" },
-            {18, "S" },
-            {19, "T" },
-            {20, "U" },
-            {21, "V" },
-            {22, "W" },
-            {23, "X" },
-            {24, "Y" },
-            {25, "Z" }
-        };
 
         public Form1()
         {
@@ -195,21 +166,14 @@ namespace CSV2QR
         /// <returns></returns>
         private ImageFormat GetImageFormat()
         {
-            ImageFormat format;
             IEnumerable<RadioButton> controls = qrCodeImageFormatGroupBox.Controls.Cast<RadioButton>();
             RadioButton checkedControl = controls.First(c => c.Checked);
-            switch (checkedControl.Text)
+            ImageFormat format = checkedControl.Text switch
             {
-                case "JPEG":
-                    format = ImageFormat.Jpeg;
-                    break;
-                case "PNG":
-                    format = ImageFormat.Png;
-                    break;
-                default:
-                    format = ImageFormat.Jpeg;
-                    break;
-            }
+                "JPEG" => ImageFormat.Jpeg,
+                "PNG" => ImageFormat.Png,
+                _ => ImageFormat.Jpeg,
+            };
             return format;
         }
 
